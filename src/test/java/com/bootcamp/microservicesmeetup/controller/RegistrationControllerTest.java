506f4e6d38d3d13a89224ece484f65e2ec1ea5b8
JoanaNahorny <joananahorny@gmail.com>
2022-04-26 22:07:10 -0300

    Test Controller

---
+++ src/test/java/com/bootcamp/microservicesmeetup/controller/RegistrationControllerTest.java
@@ -0,0 +1,71 @@
+package com.bootcamp.microservicesmeetup.controller;
+
+import com.bootcamp.microservicesmeetup.controller.dto.RegistrationDTO;
+import com.bootcamp.microservicesmeetup.model.etity.Registration;
+import com.bootcamp.microservicesmeetup.service.RegistrationService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.BDDMockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@ExtendWith(SpringExtension.class)
+@ActiveProfiles("test")
+@WebMvcTest(controllers = {RegistrationController.class})
+@AutoConfigureMockMvc
+public class RegistrationControllerTest {
+
+    static String REGISTRATION_API = "/api/registration";
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @MockBean
+    RegistrationService registrationService;
+
+    @Test
+    @DisplayName("Should create a registration with success")
+    public void createRegistrationTest() throws Exception {
+
+        RegistrationDTO registrationDTOBuilder = createNewRegistration();
+        Registration savedRegistration = Registration.builder().id(101)
+                .name("Ana").dateOfRegistration("10/10/2021").registration("001").build();
+
+        BDDMockito.given(registrationService.save(any(Registration.class))).willReturn(savedRegistration);
+
+        String json = new ObjectMapper().writeValueAsString(registrationDTOBuilder);
+
+        MockHttpServletRequestBuilder request = MockMvcRequestBuilders
+                .post(REGISTRATION_API)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(json);
+
+        mockMvc
+                .perform(request)
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("id").value(101))
+                .andExpect(jsonPath("name").value(registrationDTOBuilder.getName()))
+                .andExpect(jsonPath("dateOfRegistration").value(registrationDTOBuilder.getDateOfRegistration()))
+                .andExpect(jsonPath("registration").value(registrationDTOBuilder.getRegistration()));
+    }
+
+    private RegistrationDTO createNewRegistration() {
+        return RegistrationDTO.builder().id(101)
+                .name("Ana").dateOfRegistration("10/10/2021").registration("001").build();
+    }
+
+}
